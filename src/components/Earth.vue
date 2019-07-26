7b9ff5ef0d804f9c5b3622a6eaae5ca844205dc6
Sean McPherson <seanmcp25@gmail.com>
2019-07-25 22:48:59 -0400

    Add first Vue component!

---
+++ src/components/Earth.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{greeting}} Earth!</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: "None"
+    };
+  },
+  mounted() {
+    this.greeting = "Hello";
+  }
+};
+</script>
